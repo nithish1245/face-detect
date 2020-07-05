c634485b7944ded9704c87c64b8153e8b82216e1
nithish1245 <nithishdhanapal21@gmail.com>
2020-07-05 20:15:44 +0530

    Updates

---
+++ precache-manifest.8dfe5aa6cd391572598b006b72f549a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfd8da98e1692f29d5e18ddbbf09b17",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "c65a62ab3294ca4e74d1",
+    "url": "/face-detect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "35454728d8547df29dfd",
+    "url": "/face-detect/static/css/main.563665f6.chunk.css"
+  },
+  {
+    "revision": "c65a62ab3294ca4e74d1",
+    "url": "/face-detect/static/js/2.9b048a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detect/static/js/2.9b048a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35454728d8547df29dfd",
+    "url": "/face-detect/static/js/main.9745f600.chunk.js"
+  },
+  {
+    "revision": "222e7e7060c17ee90085",
+    "url": "/face-detect/static/js/runtime-main.e8a0c4bc.js"
+  }
+]);
